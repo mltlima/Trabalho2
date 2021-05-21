281e8f418cd0d1420fa58237ce98c6ac3675e896
mltlima <54447895+mltlima@users.noreply.github.com>
2021-05-21 16:34:16 -0300

    alarme completado

--- arquivo.c
+++ arquivo.c
@@ -11,6 +11,8 @@ int leitores_count = 0; //contador de threads lendo
 int escritores_count = 0; //contador de threads escrevendo
 int leituras = 0;
 int escritas = 0;
+int somatorio = 0;
+int media = 0;
 unsigned int shared_var = 0;
 
 medicao temperaturas[60];
@@ -29,8 +31,8 @@ int medirTemperatura(int id){
     novaTemperatura.temperatura = temperatura;
     novaTemperatura.idSensor = id;
 
-    temperaturas[escritas % 61] = novaTemperatura;
-    ultimasTemperaturas[escritas % 16] = novaTemperatura;
+    temperaturas[escritas % 60] = novaTemperatura;
+    ultimasTemperaturas[escritas % 15] = novaTemperatura;
 
     
 
@@ -43,14 +45,13 @@ int lerTemperatura(int id){
     int temperaturaLida = 0;
 
     //procura entrea as ultimas (quantidade de escritores) leituras o id 
-    for (int i = (escritas % 16) ; i >= 0; i--){
-        
-        if (ultimasTemperaturas[i].idSensor == id){
+    for (int i = ((escritas-LEITORES) % 60) ; i < leituras+LEITORES; i++){
+        if (temperaturas[i].idSensor == id){
             //leitor com mesmo sensor do escritor achado
             //printf("teste ---------> %d \n", ultimasTemperaturas[i].temperatura);
-            temperaturaLida = ultimasTemperaturas[i].temperatura;
+            temperaturaLida = temperaturas[i].temperatura;
             //passa o id da leitura
-            ultimasTemperaturas[i].idLeitura = leituras;
+            temperaturas[i].idLeitura = leituras;
             break;
         }
         
@@ -62,13 +63,36 @@ int lerTemperatura(int id){
 
 
 void alarme(){
+    //int porque = (escritas-15-LEITORES);
+    int id = (((escritas-15-LEITORES) % 15) + 15) % 15;
 
     int amarelo = 0;
     int vermelho = 0;
-    
-    for (int i = 16 ; i >= 0; i--){
-        
+
+    for (int i = 1 ; i <  16; i++){
+
+        //printf("id         %d\n",id);
+        //printf("Teste alarme ---------------> %d\n",ultimasTemperaturas[id].temperatura);
+        id = (((escritas-15-LEITORES+i) % 15)+15)%15 ;  
+        int temperatura = ultimasTemperaturas[id].temperatura; 
+
+        if (temperatura > 35){
+            amarelo++;
+            vermelho++;
+        }else {
+            vermelho = 0;
+        }
     }
+
+    if (vermelho >= 5){
+        printf("ALERTA VERMELHO\n");
+    } else if (amarelo > 5){
+        printf("ALERTA AMARELO\n");
+    } else {
+        printf("ALERTA NORMAL\n");
+    }
+    
+    
 }
 
 
@@ -78,13 +102,13 @@ void * leitor (void * arg) {
     int id = *((int*)arg);
     int temperatura;
 
-    for(int i = 1; i >= 0; i++){
+    for(int i = 0; i >= 0; i++){
         
         /* Enter critical section: */
         pthread_mutex_lock(&mutex);
         {
             pthread_cond_wait(&cond_leit, &mutex);
-            sleep(2);
+            //sleep(2);
             leitores_count ++;
             temperatura = lerTemperatura(id);
             leituras++;
@@ -93,14 +117,18 @@ void * leitor (void * arg) {
             if (!temperatura){
                 printf("erro leitura de temperatura\n");
             }
+
+            somatorio += temperatura;
+            media = somatorio/leituras;
             
-            printf("LEITOR %i | Temperatura lida : %i | Readers count: %i ||| Ciclo: %i\n",
-                    id, temperatura, leitores_count, i);
+            printf("LEITOR %i | Temperatura lida : %i | Media: %i ||| Ciclo: %i\n",
+                    id, temperatura, media, i);
         }
         pthread_mutex_unlock(&mutex);
 
         if (leitores_count == ESCRITORES){
         pthread_mutex_lock(&mutex);
+            alarme();
             pthread_cond_broadcast(&cond_escr);
             leitores_count = 0;
         pthread_mutex_unlock(&mutex);
@@ -130,16 +158,16 @@ void * escritor (void * arg) {
     int id = *((int*)arg);
     int temperatura = 0;
     //int *id = (int *) arg;
-    for(int i = 1; i >= 0; i++){
+    for(int i = 0; i >= 0; i++){
 
         
        
         /* Enter critical section: */
         pthread_mutex_lock(&mutex);
         {
-            sleep(1);
-            escritas++;
+            //sleep(1);
             temperatura = medirTemperatura(id);
+            escritas++;
             //count++;
             escritores_count++;
             shared_var++;
